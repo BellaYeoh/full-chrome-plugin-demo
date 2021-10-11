b30ee0445b62426f2e8a9eed6e41485b0aaa97dc
bellayeoh <yy1336316@126.com>
2021-10-11 14:00:32 +0800

    imporved notification demo

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-console.log('你好，我是popup!');
+document
+  .getElementById('notify')
+  .addEventListener('click', function () {
+    chrome.notifications.create(null, {
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: '这是标题',
+      message: '发送一条桌面通知',
+    });
+  });
