9a245da1b50dc630d8f2742aa9c744b05e9c9246
bellayeoh <yy1336316@126.com>
2021-10-11 13:47:01 +0800

    ominibox demo

---
+++ js/background.js
@@ -0,0 +1,83 @@
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  console.log('inputChanged: ' + text);
+
+  if (!text) {
+    return;
+  }
+
+  if (text === '美女') {
+    suggest([
+      { content: '中国' + text, description: '你要找“中国美女”吗？' },
+      { content: '日本' + text, description: '你要找“日本美女”吗？' },
+      {
+        content: '泰国' + text,
+        description: '你要找“泰国美女或人妖”吗？',
+      },
+      { content: '韩国' + text, description: '你要找“韩国美女”吗？' },
+    ]);
+  } else if (text == '微博') {
+    suggest([
+      { content: '新浪' + text, description: '新浪' + text },
+      { content: '搜狐' + text, description: '搜索' + text },
+    ]);
+  } else {
+    suggest([
+      {
+        content: '百度搜索 ' + text,
+        description: '百度搜索 ' + text,
+      },
+      {
+        content: '谷歌搜索 ' + text,
+        description: '谷歌搜索 ' + text,
+      },
+    ]);
+  }
+});
+
+chrome.omnibox.onInputEntered.addListener(text => {
+  console.log('inputEntered: ' + text);
+
+  if (!text) {
+    return;
+  }
+
+  var href = '';
+
+  if (text.endsWith('美女')) {
+    href =
+      'http://image.baidu.com/search/index?tn=baiduimage&ie=utf-8&word=' +
+      text;
+  } else if (text.startsWith('百度搜索')) {
+    href =
+      'https://www.baidu.com/s?ie=UTF-8&wd=' +
+      text.replace('百度搜索 ', '');
+  } else if (text.startsWith('谷歌搜索')) {
+    href =
+      'https://www.google.com.tw/search?q=' +
+      text.replace('谷歌搜索 ', '');
+  } else {
+    href = 'https://www.baidu.com/s?ie=UTF-8&wd=' + text;
+  }
+
+  openUrlCurrentTab(href);
+});
+
+function openUrlCurrentTab(url) {
+  getCurrentTabId(tabId => {
+    chrome.tabs.update(tabId, { url });
+  });
+}
+
+function getCurrentTabId(callback) {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      if (callback) {
+        callback(tabs.length ? tabs[0].id : null);
+      }
+    }
+  );
+}
