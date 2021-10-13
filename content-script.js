33e99ff23fd72009110f504b67bdced33511023c
bellayeoh <yy1336316@126.com>
2021-10-13 17:14:44 +0800

    connect demo

--- content-script.js
+++ content-script.js
@@ -1,3 +1,13 @@
-(function () {
-  console.log('This is content-script of simple-crx-demo!');
-})();
+// listen connect
+chrome.runtime.onConnect.addListener(function (port) {
+  console.log(port);
+
+  if (port.name === 'test-connect') {
+    port.onMessage.addListener(function (msg) {
+      console.log('收到长连接消息', msg);
+      if (msg.question === '你好，请问你是？') {
+        port.postMessage({ answer: '我是你爸！' });
+      }
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,1 +1,30 @@
-console.log('你好，我是popup!');
+// popup send message to content-scripts by connect
+document
+  .getElementById('connectToContent')
+  .addEventListener('click', function () {
+    getCurrentTabId(tabId => {
+      const port = chrome.tabs.connect(tabId, {
+        name: 'test-connect',
+      });
+
+      port.postMessage({ question: '你好，请问你是？' });
+      port.onMessage.addListener(function (msg) {
+        alert('收到消息：' + msg.answer);
+
+        if (msg.answer && msg.answer.startsWith('我是')) {
+          port.postMessage({ question: '哦，原来是你啊！' });
+        }
+      });
+    });
+  });
+
+function getCurrentTabId(callback) {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (tabs) {
+      if (callback) {
+        callback(tabs.length ? tabs[0].id : null);
+      }
+    }
+  );
+}
